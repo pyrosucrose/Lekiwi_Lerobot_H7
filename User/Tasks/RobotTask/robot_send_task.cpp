b4e35292da7842f5373dfd6f704870f14f1995dd
pyrosucrose <3183861264@qq.com>
2026-05-07 21:58:18 +0800

    [test] 添加部分测试用代码，几乎无实际作用

--- User/Tasks/RobotTask/robot_send_task.cpp
+++ User/Tasks/RobotTask/robot_send_task.cpp
@@ -18,13 +18,15 @@ void RobotSendTask(void *pv)
     {
         // auto s = Delay::GetTimeStamp();
         robot_lekiwi.SendControl();
-        // usart_printf("%d,%d,%d,%d,%d\n",
-        //     MotorSts::motors_[5]->zero_point_ecd_,
-        //     MotorSts::motors_[5]->min_pos_ecd_,
-        //     MotorSts::motors_[5]->max_pos_ecd_,
-        //     MotorSts::motors_[5]->soft_min_pos_ecd_,
-        //     MotorSts::motors_[5]->soft_max_pos_ecd_
-        //     );
+        // usart_printf("%f,%f,%f,%f,%f,%f\n",
+        //     MotorSts::motors_[MotorSts::motors_idx_[11]]->GetSoftPos_One(),
+        //     MotorSts::motors_[MotorSts::motors_idx_[12]]->GetSoftPos_One(),
+        //     MotorSts::motors_[MotorSts::motors_idx_[13]]->GetSoftPos_One(),
+        //     MotorSts::motors_[MotorSts::motors_idx_[14]]->GetSoftPos_One(),
+        //     MotorSts::motors_[MotorSts::motors_idx_[15]]->GetSoftPos_One(),
+        //     MotorSts::motors_[MotorSts::motors_idx_[16]]->GetSoftPos_One()
+        // );
+
         // MotorSts::ReadAll(MotorSts::REG::NOW_POS_L, MotorSts::REG::NOW_TEMP);
         // usart_printf("%f\n",Delay::CalculateInterval_us(s));
         // usart_printf("%d,%d,%d,%d,%d,%d,%d,%d,%d\n",
@@ -37,6 +39,26 @@ void RobotSendTask(void *pv)
         //     MotorSts::motors_[6]->pos_ecd_,
         //     MotorSts::motors_[7]->pos_ecd_,
         //     MotorSts::motors_[8]->pos_ecd_
+        //     );
+
+        // usart_printf("%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f\n",
+        //     MotorSts::motors_[0]->GetSoftPos_One(),
+        //     MotorSts::motors_[1]->GetSoftPos_One(),
+        //     MotorSts::motors_[2]->GetSoftPos_One(),
+        //     MotorSts::motors_[3]->GetSoftPos_One(),
+        //     MotorSts::motors_[4]->GetSoftPos_One(),
+        //     MotorSts::motors_[5]->GetSoftPos_One(),
+        //     MotorSts::motors_[6]->GetSoftPos_One(),
+        //     MotorSts::motors_[7]->GetSoftPos_One(),
+        //     MotorSts::motors_[8]->GetSoftPos_One()
+        //     );
+
+        // usart_printf("%d,%d,%d,%d,%d\n",
+        //     MotorSts::motors_[0]->soft_pos_ecd,
+        //     MotorSts::motors_[0]->soft_min_pos_ecd_,
+        //     MotorSts::motors_[0]->soft_max_pos_ecd_,
+        //     MotorSts::motors_[0]->zero_point_ecd_,
+        //     MotorSts::motors_[0]->pos_ecd_
         //     );
 
         // usart_printf("%d,%d,%d,%d,%d,%d\n",
@@ -60,6 +82,30 @@ void RobotSendTask(void *pv)
         //     robot_lekiwi.arm.motors[5].soft_min_pos_ecd_,
         //     robot_lekiwi.arm.motors[5].soft_max_pos_ecd_
         //     );
+
+        // static uint16_t dum = 0; dum++;
+        // if (dum >= 100)
+        // {
+        //     static uint16_t a1 = 0,a2 = 0,a3 = 0,a4 = 0,a5 = 0,a6 = 0;
+        //     static uint16_t b1 = 11451,b2 = 11451,b3 = 11451,b4 = 11451,b5 = 11451,b6 = 11451;
+        //     a1 = std::max(a1, MotorSts::motors_[MotorSts::motors_idx_[11]]->GetHardPos_Raw());
+        //     a2 = std::max(a2, MotorSts::motors_[MotorSts::motors_idx_[12]]->GetHardPos_Raw());
+        //     a3 = std::max(a3, MotorSts::motors_[MotorSts::motors_idx_[13]]->GetHardPos_Raw());
+        //     a4 = std::max(a4, MotorSts::motors_[MotorSts::motors_idx_[14]]->GetHardPos_Raw());
+        //     a5 = std::max(a5, MotorSts::motors_[MotorSts::motors_idx_[15]]->GetHardPos_Raw());
+        //     a6 = std::max(a6, MotorSts::motors_[MotorSts::motors_idx_[16]]->GetHardPos_Raw());
+        //
+        //     b1 = std::min(b1, MotorSts::motors_[MotorSts::motors_idx_[11]]->GetHardPos_Raw());
+        //     b2 = std::min(b2, MotorSts::motors_[MotorSts::motors_idx_[12]]->GetHardPos_Raw());
+        //     b3 = std::min(b3, MotorSts::motors_[MotorSts::motors_idx_[13]]->GetHardPos_Raw());
+        //     b4 = std::min(b4, MotorSts::motors_[MotorSts::motors_idx_[14]]->GetHardPos_Raw());
+        //     b5 = std::min(b5, MotorSts::motors_[MotorSts::motors_idx_[15]]->GetHardPos_Raw());
+        //     b6 = std::min(b6, MotorSts::motors_[MotorSts::motors_idx_[16]]->GetHardPos_Raw());
+        //
+        //     usart_printf("%d,%d,%d,%d,%d,%d; %d,%d,%d,%d,%d,%d\n",
+        //         a1,a2,a3,a4,a5,a6,b1,b2,b3,b4,b5,b6);
+        // }
+
         vTaskDelayUntil(&current_time, pdMS_TO_TICKS(TASK_ROBOT_SEND_TASK_PERIOD));
     }
 }
--- User/main.cpp
+++ User/main.cpp
@@ -24,11 +24,36 @@ void User_Init()
     BSP_Init();
     Buzzer::SetTone(0);
     auto s = Delay::GetTimeStamp();
+    MotorSts motor1(11,2035,720,3440,false);
+    MotorSts motor2(12,1906,830,3200,false);
+    MotorSts motor3(13,2457,883,3080,false);
+    MotorSts motor4(14,1671,862,2900,false);
+    MotorSts motor5(15,1503,300,3980,false);
+    MotorSts motor6(16,1702,1400,2870,false);
     while (true)
     {
+        HAL_Delay(10);
+        // motor1.TransmitWriteCommand(MotorSts::REG::LOCK, 0); HAL_Delay(1); // motor1.TransmitWriteCommand(MotorSts::REG::ID, 11); HAL_Delay(1);
+        // motor2.TransmitWriteCommand(MotorSts::REG::LOCK, 0); HAL_Delay(1); // motor2.TransmitWriteCommand(MotorSts::REG::ID, 12); HAL_Delay(1);
+        // motor3.TransmitWriteCommand(MotorSts::REG::LOCK, 0); HAL_Delay(1); // motor3.TransmitWriteCommand(MotorSts::REG::ID, 13); HAL_Delay(1);
+        // motor4.TransmitWriteCommand(MotorSts::REG::LOCK, 0); HAL_Delay(1); // motor4.TransmitWriteCommand(MotorSts::REG::ID, 14); HAL_Delay(1);
+        // motor5.TransmitWriteCommand(MotorSts::REG::LOCK, 0); HAL_Delay(1); // motor5.TransmitWriteCommand(MotorSts::REG::ID, 15); HAL_Delay(1);
+        // motor6.TransmitWriteCommand(MotorSts::REG::LOCK, 0); HAL_Delay(1); // motor6.TransmitWriteCommand(MotorSts::REG::ID, 16); HAL_Delay(1);
+        MotorSts::ReadAll(MotorSts::REG::NOW_POS_L, MotorSts::REG::NOW_TEMP);
+        HAL_Delay(10);
+        // motor1.AddReadRange(MotorSts::REG::NOW_POS_L, MotorSts::REG::NOW_TEMP); motor1.TransmitReadCommand();
+        usart_printf("%f,%f,%f,%f,%f,%f\n",
+            motor1.GetSoftPos_One(),
+            motor2.GetSoftPos_One(),
+            motor3.GetSoftPos_One(),
+            motor4.GetSoftPos_One(),
+            motor5.GetSoftPos_One(),
+            motor6.GetSoftPos_One()
+            );
+        MotorSts::UnpackAll();
         // Delay::Delay_ms(1000);
-        usart_printf("1\n");
-        Delay::DelayUntil_us(&s,50000);
+        // usart_printf("1\n");
+        // Delay::DelayUntil_us(&s,50000);
         // for (uint32_t i = 0; i < 1000000; i++){}
         // usart_printf("%f\n",Delay::CalculateInterval_us(s));
     }
